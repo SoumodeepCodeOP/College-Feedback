c5a1e993c12a8ca6f29259d6ef0dcc89bba78045
Soumodeep Halder <soumodeephalder8@gmail.com>
2024-02-29 14:31:38 +0530

    second commit

--- register.php
+++ register.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
-        <link rel="stylesheet" href="css/register.css">
+        <link rel="stylesheet" href="css/reg.css">
         <link rel="icon" href="ece logo.png" type="image/x-icon">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
